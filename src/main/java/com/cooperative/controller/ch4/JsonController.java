4b51ffa492f7b57f808ab0dcacc9dc9b2c45f3fa
aszd456 <602419030@qq.com>
2020-03-06 00:51:59 +0800

    json

---
+++ src/main/java/com/cooperative/controller/ch4/JsonController.java
@@ -0,0 +1,8 @@
+package com.cooperative.controller.ch4;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class JsonController {
+
+}
--- src/main/java/com/cooperative/entity/BaseMode.java
+++ src/main/java/com/cooperative/entity/BaseMode.java
@@ -2,9 +2,11 @@ package com.cooperative.entity;
 
 import lombok.Data;
 
+import javax.persistence.MappedSuperclass;
 import java.util.Date;
 
 @Data
+@MappedSuperclass
 public class BaseMode {
 
     private Integer id;
